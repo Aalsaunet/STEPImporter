c0ba8717cb0ec7252db60fb3281509a199b385ce
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2018-03-09 12:00:10 +0100

    Created operator overloads for the Vector3 structed to be used during an insertSorted and binary search function. Also refactored the code.

---
+++ STEPImport/SimpleVectors.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "stdafx.h"
+#include "SimpleVectors.h"
+
+void Vector3::Set(Standard_Real x_new, Standard_Real y_new, Standard_Real z_new) {
+	x = x_new;
+	y = y_new;
+	z = z_new;
+}
+
+Vector3 Vector3::operator-(const Vector3& v) {
+	Vector3 result;
+	result.x = this->x - v.x;
+	result.y = this->y - v.y;
+	result.z = this->z - v.z;
+	return result;
+}
+
+bool Vector3::operator==(const Vector3& v) {
+	if (this->x == v.x && this->y == v.y && this->z == v.z)
+		return true;
+	return false;
+}
+
+bool Vector3::operator>(const Vector3& v) {
+	if (this->x > v.x)
+		return true;
+	else if (this->x < v.y)
+		return false;
+	else if (this->y > v.y)
+		return true;
+	else if (this->y < v.y)
+		return false;
+	else if (this->z > v.z)
+		return true;
+	else
+		return false;
+}
+
+Vector3 Vector3::CrossProduct(Vector3 b) {
+	// Given vector a and b, their cross product is: (a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)
+	Vector3 result;
+	result.Set(this->y * b.z - this->z * b.y, this->z * b.x - this->x * b.z, this->x * b.y - this->y * b.x);
+	return result;
+}
