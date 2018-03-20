fd7419213e233ca1b712e08adfdf6c03895350f9
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2018-03-20 18:46:36 +0100

    Added a recursive function, which still needs some work

--- STEPImport/STEPImport.h
+++ STEPImport/STEPImport.h
@@ -3,6 +3,7 @@
 #define STEP_API extern "C" __declspec(dllexport)
 
 STEP_API int ImportSTEPFile(const char* filepath, char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
+void IterateShapeHierarchy(std::vector<TopoDS_Shape>& mainSubShapeList, TopoDS_Shape& shape);
 void MarshalGeometryData(char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
 void RetrieveFaces(TopoDS_Shape &shape);
 void RemoveDuplicateVerticesAndUpdateIndices();
