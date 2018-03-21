a866464a9569264d782f918e80a301da1b47d18a
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2018-03-21 13:23:53 +0100

    Restructured STEPImporter so each subshape can be processed individually

--- STEPImport/STEPImport.h
+++ STEPImport/STEPImport.h
@@ -2,11 +2,12 @@
 
 #define STEP_API extern "C" __declspec(dllexport)
 
-STEP_API int ImportSTEPFile(const char* filepath, char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
-std::vector<TopoDS_Shape> ExpandShapeHierarchy(TopoDS_Shape& rootShape);
-void MarshalGeometryData(char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
+STEP_API int ImportSTEPFile(const char* filepath, int32_t* numberOfSubShapes);
+void FindAllSubShapes(int32_t* numberOfSubShapes);
+STEP_API int ProcessSubShape(int32_t* subshapeIndex, char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
 void RetrieveFaces(TopoDS_Shape &shape);
 void RemoveDuplicateVerticesAndUpdateIndices();
+void MarshalGeometryData(char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
 void ClearGeometryData();
 int testGet3DGeometry();
 int main();
