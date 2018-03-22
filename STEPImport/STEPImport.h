a612369f56fe5e2fcab1e899649a9cf03369f6f7
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2018-03-22 19:13:37 +0100

    The models subshapes/solids are now processed and transfered independently

--- STEPImport/STEPImport.h
+++ STEPImport/STEPImport.h
@@ -4,10 +4,10 @@
 
 STEP_API int ImportSTEPFile(const char* filepath, int32_t* numberOfSubShapes);
 void FindAllSubShapes(int32_t* numberOfSubShapes);
-STEP_API int ProcessSubShape(int32_t* subshapeIndex, char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
+STEP_API int ProcessSubShape(int32_t subshapeIndex, char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
 void RetrieveFaces(TopoDS_Shape &shape);
 void RemoveDuplicateVerticesAndUpdateIndices();
 void MarshalGeometryData(char** vertexBuffer, int32_t* vertexEntryCount, char** indexbuffer, int32_t* indexCount);
 void ClearGeometryData();
-int testGet3DGeometry();
+void TestStepImporter();
 int main();
